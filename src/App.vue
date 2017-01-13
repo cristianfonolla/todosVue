85e385f3c6a1e964af30e36911b7aba932ffced6
cristianfonolla <cristian.fonolla@gmail.com>
2017-01-13 16:27:24 +0100

    routes and new todos and tokens views

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,31 @@
       <h2 class="md-title">todosVue</h2>
     </md-toolbar>
 
+
     <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+
       <md-toolbar class="md-large">
+
         <div class="md-toolbar-container">
+
           <h3 class="md-title">todosVue</h3>
+
         </div>
       </md-toolbar>
 
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi cupiditate esse necessitatibus beatae nobis, deserunt ut est fugit, tempora deleniti, eligendi commodi doloribus. Nemo, assumenda possimus, impedit inventore perferendis iusto!</p>
+      <md-list>
+        <md-list-item>
+         <md-icon>view_list</md-icon>
+          <router-link to="/todos">todosVue</router-link>
+        </md-list-item>
+
+        <md-list-item>
+         <md-icon>security</md-icon>
+          <router-link to="/tokens">Tokens</router-link>
+        </md-list-item>
+      </md-list>
     </md-sidenav>
+    TODO contingut
   </div>
 </template>
 
