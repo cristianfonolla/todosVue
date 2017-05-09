b10e1457738adfe438bb7810d63bb87fc94f8b02
Cristian Fonolla <cristianfonolla@homestead>
2017-05-09 14:34:23 +0000

    sync2

--- src/todosVue.js
+++ src/todosVue.js
@@ -1,8 +1,8 @@
 const todosVue = {
   STORAGE_KEY: 'todosvue_token',
   AUTH_CLIENT_ID: '3',
-  AUTH_REDIRECT_URI: 'http://localhost:8095/login',
-  // AUTH_REDIRECT_URI: 'http://todosvue.cristianfonolla.2dam.acacha.org/login',
+  // AUTH_REDIRECT_URI: 'http://localhost:8095/login',
+  AUTH_REDIRECT_URI: 'http://todosvue.cristianfonolla.2dam.acacha.org/login',
   API_PROFILE_URL: 'http://todosbackend.cristianfonolla.2dam.acacha.org:8080/api/v1/user',
   ANDROID_SENDER_ID: '1066055111450',
   REGISTER_GCM_TOKEN_URL: 'http://todosbackend.cristianfonolla.2dam.acacha.org:8080/api/v1/gcmtoken',
